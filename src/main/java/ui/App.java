1b140215190aeb827870124e2ec5429954e686e2
hemu <hemu@cmschina.com.cn>
2019-01-18 16:56:58 +0800

    ui

---
+++ src/main/java/ui/App.java
@@ -0,0 +1,8 @@
+package ui;
+
+public class App {
+  public static void main(String[] args) {
+    MyFrame frame = new MyFrame(800, 600);
+    frame.init();
+  }
+}
---
+++ src/main/java/ui/MyFrame.java
@@ -0,0 +1,27 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Panel;
+
+public class MyFrame {
+  private int width;
+  private int height;
+
+  public MyFrame(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public void init() {
+    Frame frame = new Frame();
+    frame.setTitle("Hello");
+    frame.setSize(new Dimension(width, height));
+    Panel pane = new Panel();
+    pane.setSize(width, height);
+    frame.add(pane);
+    pane.setBackground(Color.RED);
+    frame.setVisible(true);
+  }
+}
